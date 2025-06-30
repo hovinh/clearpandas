599a4fb356b94d105451f0ae31e8fbb04fa762c8
Ho Xuan Vinh <hovinh39@gmail.com>
2025-06-30 12:56:05 +0000

    update: simplify transform reference

--- clearpandas/pipeline/data_engineering.py
+++ clearpandas/pipeline/data_engineering.py
@@ -2,9 +2,7 @@ from typing import Dict
 
 import pandas as pd
 from clearpandas.pipeline.template import Pipeline
-from clearpandas.transform import base as base_transform
-from clearpandas.transform import customers as customers_transform
-from clearpandas.transform import orders as orders_transform
+from clearpandas import transform 
 
 """Data Engineering Pipeline
 This pipeline extracts customer and order data, transforms it to filter urgent orders,
@@ -16,11 +14,11 @@ class DataEngineeringPipeline(Pipeline):
     def extract(self) -> Dict[str, pd.DataFrame]:
         customer_connector = self.connector_factory.create_connector('data/customers.csv')
         customer_table = customer_connector.get_data("SELECT * FROM df WHERE c_acctbal > 200")
-        customer_table = base_transform.remove_nulls(customer_table)
+        customer_table = transform.base.remove_nulls(customer_table)
 
         order_connector = self.connector_factory.create_connector('data/orders.csv')
         order_table = order_connector.get_data()
-        order_table = base_transform.remove_nulls(order_table)
+        order_table = transform.base.remove_nulls(order_table)
         data = {
             'customers': customer_table,
             'orders': order_table,
@@ -30,13 +28,13 @@ class DataEngineeringPipeline(Pipeline):
     def transform(self, data: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
         customer_table = (
             data['customers']
-            .pipe(customers_transform.round_up_acctbal)
-            .pipe(customers_transform.filter_customers, max_custkey=1000)
+            .pipe(transform.customers.round_up_acctbal)
+            .pipe(transform.customers.filter_customers, max_custkey=1000)
             )
 
         order_table = (
             data['orders']
-            .pipe(orders_transform.filter_urgent_orders)
+            .pipe(transform.orders.filter_urgent_orders)
         )
 
         urgent_order_details_table = pd.merge(
